73b4524bf989cee202667422d1715e7df6dc3934
dylanbram354 <dylanbram354@gmail.com>
2021-08-11 15:58:49 -0400

    Implemented DRF token authorization for user login

--- drf_user_groups/drf_api/urls.py
+++ drf_user_groups/drf_api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from rest_framework.authtoken import views as drfviews
 from . import views
 
 urlpatterns = [
-    path('register/', views.UserView.as_view())
+    path('register/', views.UserView.as_view()),
+    path('api-token-auth/', drfviews.obtain_auth_token)
 ]
