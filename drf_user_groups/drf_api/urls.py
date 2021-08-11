681ab23a32b991c0e2bdbd44db8020bc786a1f75
dylanbram354 <dylanbram354@gmail.com>
2021-08-11 16:30:47 -0400

    Implemented token authorization on a particular endpoint

--- drf_user_groups/drf_api/urls.py
+++ drf_user_groups/drf_api/urls.py
@@ -3,6 +3,7 @@ from rest_framework.authtoken import views as drfviews
 from . import views
 
 urlpatterns = [
-    path('register/', views.UserView.as_view()),
+    path('register/', views.RegisterView.as_view()),
+    path('users/', views.UserView.as_view()),
     path('api-token-auth/', drfviews.obtain_auth_token)
 ]
--- drf_user_groups/drf_api/views.py
+++ drf_user_groups/drf_api/views.py
@@ -4,11 +4,13 @@ from .serializers import UserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
 
 
-class UserView(APIView):
+class RegisterView(APIView):
 
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -16,6 +18,11 @@ class UserView(APIView):
             serializer.create(request.data)
             return Response({'username': serializer.data['username']}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
 
     def get(self, request):
         users = User.objects.all()
